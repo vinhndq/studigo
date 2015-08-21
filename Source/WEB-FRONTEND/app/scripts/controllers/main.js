5e447045454e729e88fb884c63eccd03c342059e
VinhNDQ <quangvinh.itbk@gmail.com>
2015-08-21 23:29:16 +0700

    change v-0.2

---
+++ Source/WEB-FRONTEND/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('testApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'SitePoint'
+    ];
+  });
